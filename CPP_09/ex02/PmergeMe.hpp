3076d4ba378fd4727a830f4e9991d37212b36243
mbelouar <simobelouarraq5@gmail.com>
2024-10-23 17:07:23 +0200

    start implementing the .hpp

--- CPP_09/ex02/PmergeMe.hpp
+++ CPP_09/ex02/PmergeMe.hpp
@@ -3,11 +3,34 @@
 
 # include <iostream>
 # include <vector>
-# include <algorithm>
-# include <iterator>
 # include <list>
 # include <string>
-# include <sstream>
 # include <stdexcept>
+# include <chrono>
+
+class PmergeMe {
+    private:
+        std::vector<int> _vec;
+        std::list<int> _list;
+
+        // Pushes a number to both containers
+        bool addNumberToContainers(int number);
+
+        // Sorting algorithms for vector
+        void sortVectorUsingInsertion(int first, int last);
+        void sortVectorUsingMerge(int first, int last);
+        void mergeVectorSections(int first, int mid, int last);
+
+        // Sorting algorithms for list
+        void sortListUsingInsertion(int first, int last);
+        void sortListUsingMerge(int first, int last);
+        void mergeListSections(int first, int mid, int last);
+
+    public:
+        PmergeMe();
+        PmergeMe(const PmergeMe& src);
+        ~PmergeMe();
+        PmergeMe& operator=(const PmergeMe& rhs);
+};
 
 #endif
